8b44505d918e9b65ffc442f17265102067c4c981
KellyRudnicki <kelly.rudnicki@comcast.net>
2019-12-30 15:39:02 -0700

    first attempt

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Snowflake.create({shape: "Hexagonal Plates"})
+Snowflake.create({shape: "Stellar Plates"})
+Snowflake.create({shape: "Stellar Dendrites"})
+Snowflake.create({shape: "Fernlike Stellar Dendrites"})
